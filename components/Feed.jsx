0a265559d6d93e2be7a9a3855bdd8f27dc7331cd
Somashekar B R <sanjayrao161@gmail.com>
2023-06-20 23:45:49 +0530

    create feed page with dynamic prompts loading into

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+"use client";
+import React, { useState, useEffect } from "react";
+import PromptCard from "./PromptCard";
 
-const Feed = () => {
+const PromptCardList = ({ data, handleTagClick }) => {
   return (
-    <div>Feed</div>
-  )
-}
+    <div className="mt-16 prompt_layout">
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  );
+};
+const Feed = () => {
+  const [searchText, setSearchText] = useState("");
+  const [post, setPost] = useState([]);
+  const handleSearchChange = (e) => {
+    setSearchText(e.target.value);
+  };
 
-export default Feed
+  useEffect(() => {
+    const fethcPost = async () => {
+      const response = await fetch("/api/prompt");
+      const data = await response.json();
+      setPost(data);
+    };
+    fethcPost();
+  }, []);
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or a username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+      <PromptCardList data={post} handleTagClick={() => {}} />
+    </section>
+  );
+};
+
+export default Feed;
